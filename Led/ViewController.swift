87ad26b58fa84a1ef03244c996de381d82648d71
jinunzzan <lovebox6607@gmail.com>
2023-06-05 17:15:28 +0900

    뷰컨트롤러 라이프사이클

--- Led/ViewController.swift
+++ Led/ViewController.swift
@@ -11,9 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("뷰디드로드")
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("뷰윌어피어")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print("뷰디드어피어")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print("뷰윌디사피어")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print("뷰딛디사피어")
+    }
 
     @IBAction func tapCodePushButton(_ sender: Any) {
         //스토리보드에 있는 뷰컨을 인스턴스 화 해준다
